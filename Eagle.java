8e8121cf798568ac025f22e144c98a3b5986509a
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-06-10 16:49:21 +0200

    Implements Fly method

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,39 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    @Override
+    public void takeOff() {
+        this.flying = true;
+        System.out.printf("%s takes off in the sky%n", this.getName());
+    }
+
+    @Override
+    public int ascend(int meters) {
+        this.altitude += meters;
+        System.out.printf("%s flies upward, altitude: %d%n", this.getName(), this.altitude);
+        return this.altitude;
+    }
+
+    @Override
+    public void glide() {
+        System.out.printf("glides into the air.%n");
+    }
+
+    @Override
+    public int descend(int meters) {
+        this.altitude -= meters;
+        System.out.printf("%s flies downward, altitude: %d%n", this.getName(), this.altitude);
+        return this.altitude;
+    }
+
+    @Override
+    public void land() {
+        if (this.altitude > 1) {
+            System.out.printf("%s is too high, it can't land.%n", getName());
+        } else {
+            System.out.printf("%s lands on the ground.%n", getName());
+        }
+    }
+
 }
